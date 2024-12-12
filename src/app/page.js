e3ee8374647f8d6a04fa84b7ca3c6cbff73a9eca
Sona30k <sona22kerketta@gmail.com>
2024-12-12 23:25:23 +0530

    Glimpse of last electrothon

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
+"use client";
 import React from "react";
 import Image from "next/image";
-import MainPg from '@/components/MainPg.js'
+import MainPg from '@/components/MainPg.js';
+import styles from './page.module.css'; // Import CSS module
+import Slider from '@/components/slider.js';
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <MainPg />
+      <h1 className={styles.title}>Glimpse of Last Electrothon</h1>
+      <Slider />
     </div>
   );
 }
--- src/components/MainPg.js
+++ src/components/MainPg.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import bg from '@/assets/background.png'
-import Image from 'next/image'
+import React from "react";
+import bg from "@/assets/background.png";
+import Image from "next/image";
+
+
 
 const MainPg = () => {
   return (
-    <div className=''>
-      <Image className='h-screen w-screen' src={bg} alt="" />
-    </div>
-  )
-}
+    <div className="relative">
+      {/* Full-screen Background Image */}
+      <Image
+        className="absolute top-0 left-0 h-screen w-screen object-cover z-0"
+        src={bg}
+        alt="Background"
+        priority
+      />
+      <div className="flex flex-col">
+        <h1>Glimpse of Last Electrothon</h1>
 
-export default MainPg
+      </div>
+
+
+    </div>
+  );
+};
+
+export default MainPg;
