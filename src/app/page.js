0d1d460563a0f1c603df57e02f03f8305ecb70c2
Ishita-2611 <23bec045@nith.ac.in>
2024-12-12 14:01:04 +0530

    faq section

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import MainPg from '@/components/MainPg/MainPg.js'
+import MainPg from '../components/MainPg.js'
 
 export default function Home() {
   return (
