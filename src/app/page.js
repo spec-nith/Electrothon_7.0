9916e2973b2ad265e12e04550dc1cf8d053fd03c
ArpitSiNgh08 <arpitsingh8124@gmail.com>
2024-12-13 20:20:39 +0530

    Second Commit
    Organised Folder in Components

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import MainPg from '@/components/MainPg/MainPg.js'
-import AboutUs from '@/components/MainPg/AboutUs.js'
+import AboutUs from '@/components/About_Us/AboutUs.js'
 
 export default function Home() {
   return (
