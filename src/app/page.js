9339d19c31e1b2f4b4b25f00d6d87ff12b0c1196
Sona30k <146016113+Sona30k@users.noreply.github.com>
2024-12-14 14:41:25 +0000

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import React from "react";
 import Image from "next/image";
 import MainPg from '@/components/MainPg.js';
 import styles from './page.module.css'; // Import CSS module
-import Slider from '@/components/slider.js';
+import Slider from '@/components/last_electrothon/slider.js';
 
 export default function Home() {
   return (
