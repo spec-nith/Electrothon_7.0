24b677e4c7094bc45992a527fe77980f8b560873
Akshanshkaushal <akshanshkaushal9@gmail.com>
2024-12-18 21:19:47 +0530

    solved lint errors

--- src/components/Sponsors/Sponsers1.jsx
+++ src/components/Sponsors/Sponsers1.jsx
@@ -1,23 +1,26 @@
-import Heading from "./Heading"
+import Heading from "./Heading";
 import Box from "./logoboard";
 import PartnerBox from "./PartnerBoard";
 import SubHeading from "./SubHeading";
-import logo from "../../assets/logos/th.jpg"
-const Sponsors1= () =>{
-    return(
-        
-      <div className="container flex flex-col items-center w-3/4 mx-auto bg-transparent text-white">
-        <Heading children={"OUR SPONSORS"} />
-        <div className="container flex flex-col items-center bg-transparent">
-        <SubHeading children={"Event Partner"} />
+import logo from "../../assets/logos/th.jpg";
+
+const Sponsors1 = () => {
+  return (
+    <div className="container flex flex-col items-center w-3/4 mx-auto bg-transparent text-white">
+      {/* Nest the heading content inside the Heading component */}
+      <Heading>OUR SPONSORS</Heading>
+      <div className="container flex flex-col items-center bg-transparent">
+        {/* Nest the subheading content inside the SubHeading component */}
+        <SubHeading>Event Partner</SubHeading>
         <PartnerBox />
-        </div>
-        <div className="container flex flex-col items-center bg-transparent">
-        <SubHeading children={"Chamberlain Sponsors"} />
-        <Box companyLogos={[{"logo":`${logo.src}`},]}/>
-        </div>
       </div>
-   
-    )
-}
+      <div className="container flex flex-col items-center bg-transparent">
+        {/* Nest the subheading content inside the SubHeading component */}
+        <SubHeading>Chamberlain Sponsors</SubHeading>
+        <Box companyLogos={[{ logo: `${logo.src}` }]} />
+      </div>
+    </div>
+  );
+};
+
 export default Sponsors1;
