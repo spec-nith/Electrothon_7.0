b6a16f1e7be380c44d605c44ec17d1ee1cb294d5
Ashish0243 <ashishrajpal73@gmail.com>
2024-12-18 13:26:03 +0530

    after merging spon in main

--- src/app/page.js
+++ src/app/page.js
@@ -7,25 +7,21 @@ import Sponsors1 from "@/components/Sponsors/Sponsers1";
 import Sponsors2 from "@/components/Sponsors/Sponsors2";
 import bg from "../assets/backgroundimg.jpg"
 
-
 export default function Home() {
   
   return (
 
   
     <div
-    className="relative bg-cover bg-center bg-fixed"
-    style={{ backgroundImage: `url(${bg.src})` }}
-
-  >
+    className="relative bg-cover bg-center bg-fixed h-full overflow-y-auto"
+    style={{ backgroundImage: `url(${bg.src})` }}>
     <MainPg />
-    {/* <section className="min-h-screen flex flex-col items-center  bg-transparent"> */}
+    <h1 className={styles.title}>Glimpse of Last Electrothon</h1>
+    <Slider />
     <Sponsors1 />
     <Sponsors2 />
-    {/* </section>
-     */}
-     <h1 className={styles.title}>Glimpse of Last Electrothon</h1>
-     <Slider />
+    
+    
    
   </div>
   
