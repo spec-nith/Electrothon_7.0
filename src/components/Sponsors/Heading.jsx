e2a8686181ce8160186b1e9ae48e743e615cd417
Ashish0243 <ashishrajpal73@gmail.com>
2024-12-19 12:39:55 +0530

    responsiveness of Sponsors section

--- src/components/Sponsors/Heading.jsx
+++ src/components/Sponsors/Heading.jsx
@@ -2,7 +2,7 @@ const Heading = ({ children }) => {
     return (
       <h1
         style={{ fontFamily: 'Pirata One, sans-serif' }}
-        className="text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl text-center lg:mt-4 xl:mt-5 xl:mb-20 lg:mb-10 sm:mt-3 sm:mb-15 md:mb-16 md:mt-4 bg-transparent"
+        className="text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl text-center 2xl:mt-4 xl:mt-5 lg:mt-4 md:mt-4 sm:mt-3 2xl:mb-16 xl:mb-20 lg:mb-10 md:mb-16 sm:mb-14 xs:mb-12 bg-transparent"
       >
         {children}
       </h1>
--- src/components/Sponsors/PartnerBoard.jsx
+++ src/components/Sponsors/PartnerBoard.jsx
@@ -1,15 +1,20 @@
-import partnerlogo from "../../assets/logos/mlh-yEbPA3jb.png"
-const PartnerBox=()=>{
-    return(
-        <div className="flex flex-wrap items-center justify-center gap-2 bg-bd w-1/4 mb-4 mt-0  h-28 xl:mb-8 lg:mb-2 md:mb-4 rounded-lg xl:h-40 lg:h-32 sm:min-h-28 sm:mb-6  "  >
-             <img
-            src={partnerlogo.src}
-            alt="Logo"
-            className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-30 lg:h-30 xl:w-40 xl:h-36 xl:p-2 lg:p-1 md:p-1 bg-transparent"
-          />
-        </div>
-    )
-}
+import Image from 'next/image';
+import partnerlogo from "../../assets/logos/mlh-yEbPA3jb.png";
 
+const PartnerBox = () => {
+  return (
+    <div className="flex flex-wrap items-center justify-center gap-2 bg-bd w-1/4 mb-4 mt-0 h-28 xl:mb-8 lg:mb-2 md:mb-4 sm:mb-6 rounded-lg 2xl:h-40 xl:h-40 lg:h-36 md:h-32 sm:min-h-28 xs:h-24 2xl:w-60 xl:w-60 lg:w-52 md:w-44 sm:min-w-40 xs:w-36">
+      <div className="relative 2xl:h-36 xl:h-36 lg:h-32 md:h-24 sm:h-20 xs:h-14 2xl:w-40 xl:w-40 lg:w-36 md:w-28 sm:w-28 xs:w-24 xl:p-2 lg:p-1 md:p-1 bg-transparent">
+        <Image
+          src={partnerlogo}
+          alt="Partner Logo"
+          layout="fill"
+          objectFit="contain"
+          className="rounded-lg"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default PartnerBox;
