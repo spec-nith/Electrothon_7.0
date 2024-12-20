ba25f880a7dd53ca6f903e32264a5f187ab1c479
S.P.E.C <57700332+spec-nith@users.noreply.github.com>
2024-12-21 00:28:08 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@ import MainPg from '@/components/MainPg/MainPg.js'
 import AboutUs from '@/components/About_Us/AboutUs.js' 
 import Faqs from "@/components/Faq/faq";
 import styles from './page.module.css'; // Import CSS module
-import DualSlider from '@/components/last_electrothon/slider';
+import Slider from '@/components/last_electrothon/slider.js';
 import Sponsors1 from "@/components/Sponsors/Sponsers1";
-import Sponsors2 from "@/components/Sponsors/Sponsors2"; 
+import Sponsors2 from "@/components/Sponsors/Sponsors2";
+import Prize from "@/components/Prizes/Prizes";
 import SliderT from "@/components/Testimonials/slider";
 import bg from "../assets/backgroundimg.jpg"
-import Footer from "@/components/contact_us/Footer"; 
+import Footer from "@/components/contact_us/Footer";
+import JudgesSection from "../components/JudgesSection/JudgesSection.js";
 import Image from "next/image";
 export default function Home() {
   return (
@@ -18,7 +20,9 @@ export default function Home() {
     <MainPg />
     <AboutUs />
     <h1 className={styles.title}>Glimpse of Last Electrothon</h1>
-    <DualSlider />  
+    <Slider />
+    <JudgesSection />
+    <Prize />
     <Sponsors1 />
     <Sponsors2 />
     <SliderT /> 
