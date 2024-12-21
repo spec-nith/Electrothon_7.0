23d5611b229b26cac5356c336139d395eca2d8c7
Ishita-2611 <23bec045@nith.ac.in>
2024-12-21 12:43:42 +0530

    update

---
+++ src/assets/prizes_data/prizes.js
@@ -0,0 +1,13 @@
+const data_prizes = [
+    {
+        id:"1",
+
+        pic1: "./prizes/gold.png",
+        pic2: "./prizes/silver.png",
+        pic3: "./prizes/bronze.png",
+       
+        
+    },
+];
+
+export default data_prizes;
