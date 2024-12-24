dbbb6b3c9906802da5711d3f3ef3903b4cc73d43
utkarsh <utkarahgupta111@gmail.com>
2024-12-24 21:48:21 +0530

    changed judges section added coming soon section

--- src/components/comingsoon/comingsoon.js
+++ src/components/comingsoon/comingsoon.js
@@ -1,12 +1,10 @@
-import React from 'react';
-
 const ComingSoon = () => {
     return (
-        <div className="flex items-center justify-center h-[4cm] w-[7cm] bg-cover bg-center relative text-white border rounded-lg">
+        <div className="flex items-center justify-center h-[15cm] w-[20cm] bg-cover bg-center relative text-white border rounded-lg">
             <div className="absolute inset-0 bg-black bg-opacity-50 rounded-lg"></div>
-            <div className="relative z-10 text-center p-2">
-                <h1 className="text-xl font-bold md:text-2xl">Coming Soon</h1>
-                <p className="mt-2 text-sm md:text-base">
+            <div className="relative z-10 text-center p-6">
+                <h1 className="text-3xl font-bold md:text-4xl">Coming Soon</h1>
+                <p className="mt-4 text-lg md:text-xl">
                     We're working on this. Stay tuned!
                 </p>
             </div>
