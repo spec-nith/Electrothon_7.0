a00ea896beb4b603166b6d4bef28f06500a129f7
utkarsh <utkarahgupta111@gmail.com>
2024-12-25 14:32:20 +0530

    made coming soon responsive

--- src/components/comingsoon/comingsoon.js
+++ src/components/comingsoon/comingsoon.js
@@ -1,10 +1,12 @@
 const ComingSoon = () => {
     return (
-        <div className="flex items-center justify-center h-[15cm] w-[20cm] bg-cover bg-center relative text-white border rounded-lg">
+        <div className="flex items-center justify-center h-[40vh] w-[90vw] sm:h-[50vh] sm:w-[80vw] lg:h-[60vh] lg:w-[70vw] bg-cover bg-center relative text-white border rounded-lg">
             <div className="absolute inset-0 bg-black bg-opacity-50 rounded-lg"></div>
             <div className="relative z-10 text-center p-6">
-                <h1 className="text-3xl font-bold md:text-4xl">Coming Soon</h1>
-                <p className="mt-4 text-lg md:text-xl">
+                <h1 className="text-2xl font-bold sm:text-3xl lg:text-4xl">
+                    Coming Soon
+                </h1>
+                <p className="mt-4 text-sm sm:text-base lg:text-lg">
                     We're working on this. Stay tuned!
                 </p>
             </div>
