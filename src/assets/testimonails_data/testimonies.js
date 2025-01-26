ce0ec607f60d9b888f846b60ebbb02e81aa4e11f
bhinfinity <24bec076@nith.ac.in>
2025-01-26 19:40:58 +0530

    This commit adds more testimonials to electrothon website

--- src/assets/testimonails_data/testimonies.js
+++ src/assets/testimonails_data/testimonies.js
@@ -1,3 +1,4 @@
+import styles from "@/components/Tracks_new/style";
 
 
 const Data = [
@@ -26,7 +27,79 @@ const Data = [
       content:
         "Thrilled to have participated in the electrifying Electrothon organized by SPEC_NITH at our college! It was my first hackathon and the experience was exhilarating . 🔥We participated in this prestigious hackathon where Sponsors posed diverse track challenges for participants...",
       postpic: "./testimonials/post3.png",
-    }
-    
+    },
+    {
+      id:"4",  
+      name: "Kirti Thakur",
+      profilepic: "./testimonials/kirti_pfp.jpg",
+      date: "10mo",
+      content:
+        "Wiring the future one idea at a time! 🔌#electrothon6.0 #nith",
+      postpic: "./testimonials/kirti_post.png",
+    },
+    {
+      id:"5",  
+      name: "Srishti Sharma",
+      profilepic: "./testimonials/srishti_pfp.jpg",
+      date: "10mo",
+      content:
+        "🌟 Exciting Announcement from Our Journey at Electrothon 6.0! 🌟 Introducing EcoEats - Our FoodTech Project! 🍽️ Stay tuned for more updates on EcoEats' journey beyond Electrothon 6.0! 🌐 We extend our gratitude to Electrothon 6.0 and NIT Hamirpur for an unforgettable experience! 💡✨...",
+      postpic: "./testimonials/srishti_post.png",
+    },
+    {
+      id:"6",  
+      name: "Animesh Chaudhri",
+      profilepic: "./testimonials/animesh_pfp.jpg",
+      date: "10mo",
+      content:
+        "The Rizzle Chronicles at Electrothon 6.0 organised by S.P.E.C NITH. Delighted to announce that we emerged victorious after an exhilarating 36-hour Hackathon organized by Nit Hamirpur. Our journey was filled with excitement, challenges, laughter, and an unwavering desire to innovate...",
+      postpic: "./testimonials/animesh_post.png",
+    },
+    {
+      id:"7",  
+      name: "Khyati Soni",
+      profilepic: "./testimonials/khyati_pfp.jpg",
+      date: "9mo",
+      content:
+        "Thank you to the organizers of Electrothon 6.0 SPEC_NITH Major League Hacking for providing us with this incredible opportunity, and congratulations to all the participants for their outstanding projects. Here's to many more accomplishments and exciting adventures ahead! 🎉🚀...",
+      postpic: "./testimonials/khyati_post.png",
+    },
+    {
+      id:"8",  
+      name: "Chirag Rai",
+      profilepic: "./testimonials/chirag_pfp.jpg",
+      date: "10mo",
+      content:
+        "I am thrilled to announce that our team emerged victorious at Electrothon 6.0, the 48-hour hackathon organized by S.P.E.C NITH. I am incredibly proud of our achievement and grateful for the opportunity to showcase our talent and innovation...",
+      postpic: "./testimonials/chirag_post.png",
+    },
+    {
+      id:"9",  
+      name: "Aniket Chauhan",
+      profilepic: "./testimonials/aniket_pfp.jpg",
+      date: "9mo",
+      content:
+        "Excited to dive into a 36-hour hackathon adventure at Electrothon6.0! Currently in the trenches, building something innovative and game-changing with my awesome team. Let the coding marathon begin...",
+      postpic: "./testimonials/aniket_post.png",
+    },
+    {
+      id:"10",  
+      name: "Ishaan Yadav",
+      profilepic: "./testimonials/ishaan_pfp.jpg",
+      date: "10mo",
+      content:
+        "I had an amazing experience in the ELECTROTHON 6.0 ,a great opportunity to learn and solve real world problems judged by industry professionals.🎇😊 Grateful for the opportunity to learn, grow, and connect with like-minded individuals. Looking forward to more collaborative endeavors in the future!🎉🎉...",
+      postpic: "./testimonials/ishaan_post.png", 
+    },
+    {
+      id:"11",  
+      name: "Soham Juneja",
+      profilepic: "./testimonials/soham_pfp.jpg",
+      date: "10mo",
+      content:
+        "Electrothon 6.0 at NIT Hamirpur - A whirlwind of learning and achievement!⚡ Thrilled to have participated in Electrothon 6.0 (MLH Hackathon) organised by SPEC at NIT Hamirpur as part of Team Tetratech Trailblazers. We presented our project, Campus Setu, which aims to be a one-stop solution for hostel life...",
+      postpic: "./testimonials/soham_post.png", 
+    },
+
   ];
   export default Data;
