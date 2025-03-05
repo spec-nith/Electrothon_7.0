d206f9f06b37541230ef93a58a2cc279b67b8342
ArpitSiNgh08 <147160433+ArpitSiNgh08@users.noreply.github.com>
2025-03-06 01:25:05 +0530

    Changed Sponsor Subheading font

--- src/components/Sponsors/SubHeading.jsx
+++ src/components/Sponsors/SubHeading.jsx
@@ -1,13 +1,12 @@
 const SubHeading = ({ children }) => {
-    return (
-      <h1
-        style={{ fontFamily: 'Jacques Francois, sans-serif' }}
-        className="text-4xl mb-9 mt-1 xs:text-4xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-7xl 2xl:text-7xl text-center 2xl:mt-6 xl:mt-7 lg:mt-12 md:mt-8 sm:mt-8 xs:mt-8 md:mb-8 sm:mb-7 bg-transparent"
-      >
-        {children}
-      </h1>
-    );
-  };
-  
-  export default SubHeading;
-  
+  return (
+    <h1
+      style={{ fontFamily: 'Eleanora, sans-serif' }}
+      className="text-4xl mb-9 mt-1 xs:text-4xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-7xl 2xl:text-7xl text-center 2xl:mt-6 xl:mt-7 lg:mt-12 md:mt-8 sm:mt-8 xs:mt-8 md:mb-8 sm:mb-7 bg-transparent"
+    >
+      {children}
+    </h1>
+  );
+};
+
+export default SubHeading;
